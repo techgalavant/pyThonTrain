afea4a4dbf030af94cd4681266e277850c5124a9
Mike Fallon <tech.galavant@gmail.com>
2021-03-08 17:07:58 +0000

    added some comments to pre-curso firebase integration

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 name = input('What is your name?\n')
-print('Hi, %s.' % name)
 from replit import db
+
 db["name"]= (name)
 keys = db.keys()
 matches = db.prefix("n")
 print (matches)
 value = db["name"]
 print (value)
+## https://pypi.org/project/firebase/
+## https://firebase.googleblog.com/2017/07/accessing-database-from-python-admin-sdk.html
