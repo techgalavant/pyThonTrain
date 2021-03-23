c901f88fa86ea53fb115eb3932c4d917d780ac4f
Mike Fallon <tech.galavant@gmail.com>
2021-03-23 18:21:25 +0000

    main.py updated format

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 name = input('What is your name?\n')
 from replit import db
 
-db["name"]= (name)
+db["name"] = (name)
 keys = db.keys()
 matches = db.prefix("n")
-print (matches)
+print(matches)
 value = db["name"]
-print (value)
+print(value)
 ## https://pypi.org/project/firebase/
 ## https://firebase.googleblog.com/2017/07/accessing-database-from-python-admin-sdk.html
