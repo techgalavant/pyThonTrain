45f3cc191db76856a909b82e7b3fb0f06e61a1f9
Mike Fallon <tech.galavant@gmail.com>
2021-03-25 20:09:20 +0000

    added function for db_test in main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-name = input('What is your name?\n')
-print('Hi, %s.' % name)
+#name = input('What is your name?\n')
+#print('Hi, %s.' % name)
 
 from words import fetch_words
 
 fetch_words()
+
+from db_train import db_test
+
+db_test()
