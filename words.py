9ecdd24bd235f0c30b504c4b6747c76baef9290e
Mike Fallon <tech.galavant@gmail.com>
2021-03-29 17:52:25 +0000

    added swap_dict to words.py

--- words.py
+++ words.py
@@ -5,13 +5,21 @@ from urllib.request import urlopen
 def fetch_words(url):
     story = urlopen(url)
     story_words = []
+    
+    # replace some words - with help from https://www.geeksforgeeks.org/python-replace-words-from-dictionary/
+    # replacement words
+    swap_dict = {"noisiest" : "loude$t", "everything" : "allthings", "winter" : "Wintear", "for" : "four"}
     for line in story:
         line_words = line.decode('utf-8').split()
         for word in line_words:
-            story_words.append(word)
+            
+            # find words to swap from swap_dict
+            story_words.append(swap_dict.get(word, word))
+
+            # story_words.append(word)
 
     story.close()
-    story_words[10] = 'Barsha'
+    story_words[10] = 'Barsha' # just replace the 11th word with 'Barsha'
     return story_words
 
 
