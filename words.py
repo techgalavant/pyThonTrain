fabd629716a85e57becc0629a29fa0f4db9079e8
Mike Fallon <tech.galavant@gmail.com>
2021-04-01 18:30:45 +0000

    replace a string with a number in the list

--- words.py
+++ words.py
@@ -4,22 +4,34 @@ from urllib.request import urlopen
 # URL to try http://sixty-north.com/c/t.txt
 # URL to try https://shakespeare.folger.edu/shakespeares-works/hamlet/
 
+
 def fetch_words(url):
     story = urlopen(url)
     story_words = []
-    
+
     # replacement words - with help from https://www.geeksforgeeks.org/python-replace-words-from-dictionary/
-    swap_dict = {"noisiest" : "loude$t", "everything" : "allthings", "winter" : "Wintear", "for" : "four", "it" : "IT"}
+    swap_dict = {
+        "noisiest": "loude$t",
+        "everything": "allthings",
+        "winter": "Wintear",
+        "for": "four",
+        "it": "IT"
+    }
     word_count = 0
+    swap_number = 0 # HW assignment to replace words with ints in the list
+
     for line in story:
         line_words = line.decode('utf-8').split()
         for word in line_words:
             word_count += line_words.count(word)
-            
+
             # replace every 5th word with "Barsha"
             def replace_words(word):
+                nonlocal swap_number
                 if word_count % 5 == 0:
-                    story_words.append("Barsha")
+                    # story_words.append("Barsha")
+                    swap_number += 100
+                    story_words.append(str(swap_number))
                 else:
                     # find words to swap from swap_dict
                     story_words.append(swap_dict.get(word, word))
@@ -27,10 +39,11 @@ def fetch_words(url):
             replace_words(word)
 
     story.close()
-    
+
     story_words.append(str(word_count) + " words in this file.")
 
     return story_words
+
 
 def print_items(items):
     for item in items:
@@ -43,4 +56,4 @@ def main(url):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1])  # passes the url as argv[1]
